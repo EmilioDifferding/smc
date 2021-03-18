8a2f72a9569a1ad2b789c7588929d5c119f09519
EmilioDifferding <emiliodiffer@gmail.com>
2021-03-18 18:41:13 -0300

    some adjustments and gitIgnored files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,11 +5,6 @@
         integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
         crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css">
-
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view/>
   </div>
 </template>
