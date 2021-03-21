fb3771e8d8ad3246df666c0eacbbfa476eb510ee
EmilioDifferding <emiliodiffer@gmail.com>
2021-03-21 15:27:10 -0300

    test commit

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -13,7 +13,7 @@ api = Blueprint('api', __name__)
 @api.route('/places', methods=['GET', 'POST'])
 def places():
     if request.method == 'GET':
-        places = Place.query.all()
+        places = Place.query.all() 
         return jsonify({ 'places':[p.to_dict() for p in places] })
     
     elif request.method == 'POST':
